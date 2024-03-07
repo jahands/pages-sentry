33a384d7da095b52b4b20866700d16de9eefe8cf
Greg Brimble <gbrimble@cloudflare.com>
2024-03-07 18:50:05 -0500

    Fix

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,8 +1,12 @@
 import sentryPlugin from '@cloudflare/pages-plugin-sentry'
+import { version } from '../package.json'
 
 export const onRequest: PagesFunction<{
 	SENTRY_DSN: string
 	SENTRY_RELEASE: string
 }> = (context) => {
-	return sentryPlugin({ dsn: context.env.SENTRY_DSN, release: context.env.SENTRY_RELEASE })(context)
+	return sentryPlugin({
+		dsn: 'https://a3edd40d3583695eebaa24e53140f622@o1072497.ingest.us.sentry.io/4506871980818432',
+		release: version,
+	})(context)
 }
