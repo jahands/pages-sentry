69259bde71a99cd7836939a400a5211681bb3831
Jacob Hands <jacob@gogit.io>
2024-03-07 20:53:56 -0600

    It works!!!!

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,8 +1,25 @@
 import sentryPlugin from '@cloudflare/pages-plugin-sentry'
+import { RewriteFrames } from '@sentry/integrations'
 import { version } from '../package.json'
 
+function stripPrefix(path: string): string {
+	const separator = '/static/_worker.js/../../'
+	return path.substring(path.indexOf(separator) + separator.length)
+}
 export const onRequest: PagesFunction<{
 	SENTRY_DSN: string
 }> = (context) => {
-	return sentryPlugin({ dsn: context.env.SENTRY_DSN, release: version })(context)
+	return sentryPlugin({
+		dsn: context.env.SENTRY_DSN,
+		release: version,
+		integrations: [
+			new RewriteFrames({
+				iteratee: (frame) => {
+					frame.abs_path = frame.abs_path ? stripPrefix(frame.abs_path) : frame.abs_path
+					frame.filename = frame.filename ? stripPrefix(frame.filename) : frame.filename
+					return frame
+				},
+			}),
+		],
+	})(context)
 }
